a5c50d6b639b24d5253b8882e430a745164f2955
Christian Buck <cbuck@lantis.de>
2015-05-12 19:20:16 -0500

    bitextor pipeline

--- baseline/html2bitextor.py
+++ baseline/html2bitextor.py
@@ -15,40 +15,24 @@ from chardet.universaldetector import UniversalDetector
 magic_numer = "df6fa1abb58549287111ba8d776733e9"
 
 
-def process_buffer(buf, d):
-    if not buf:
-        return
-    header = buf[0]
-    url = header.split()[1]
-    skip = 0
-    empty_lines = 0
-    while empty_lines < 2:
-        skip += 1
-        if not buf[skip].strip():
-            empty_lines += 1
-
+def process_buffer(buf):
     detector = UniversalDetector()
-    for line in buf[skip + 1:]:
+    for line in buf:
         detector.feed(line)
         if detector.done:
             break
     detector.close()
     encoding = detector.result
-    html = "".join(buf[skip + 1:])
+    html = "".join(buf)
     html = html.decode(encoding["encoding"])
-    d[url] = (header, html)
+    return html
 
 
-def read_file(f, d):
-    buf = []
+def read_file(f):
+    buf = ["source_file"]
     for line in f:
-        line = line
-        if line.startswith(magic_numer):
-            process_buffer(buf, d)
-            buf = [line]
-            continue
         buf.append(line)
-    process_buffer(buf, d)
+    return process_buffer(buf)
 
 
 def process_dict(d):
@@ -59,18 +43,28 @@ def process_dict(d):
         yield u, original_url, html, text
 
 
-def write_lett(sdict, tdict, slang, tlang, f):
+def write_lett(s, t, slang, tlang, f):
     mime_type = "text/html"
     encoding = "charset=utf-8"
-    for l, d in ((slang, sdict), (tlang, tdict)):
-        for url, original_url, html, text in process_dict(d):
-            f.write("{l}\t{mime}\t{enc}\t{name}\t{html}\t{text}\n".format(
-                l=l,
-                mime=mime_type,
-                enc=encoding,
-                name=original_url,
-                html=base64.b64encode(html.encode("utf-8")),
-                text=base64.b64encode(text.encode("utf-8"))))
+
+    stext = html2text(s.encode("utf-8"))
+    f.write("{l}\t{mime}\t{enc}\t{name}\t{html}\t{text}\n".format(
+        l=slang,
+        mime=mime_type,
+        enc=encoding,
+        name="source_file",
+        html=base64.b64encode(s.encode("utf-8")),
+        text=base64.b64encode(stext.encode("utf-8"))))
+
+    ttext = html2text(t.encode("utf-8"))
+    f.write("{l}\t{mime}\t{enc}\t{name}\t{html}\t{text}\n".format(
+        l=tlang,
+        mime=mime_type,
+        enc=encoding,
+        name="source_file",
+        html=base64.b64encode(t.encode("utf-8")),
+        text=base64.b64encode(ttext.encode("utf-8"))))
+
 
 if __name__ == "__main__":
     import argparse
@@ -85,7 +79,6 @@ if __name__ == "__main__":
     parser.add_argument('-tlang', help='target language', default='fr')
     args = parser.parse_args(sys.argv[1:])
 
-    sdict, tdict = {}, {}
-    read_file(args.source, sdict)
-    read_file(args.target, tdict)
-    write_lett(sdict, tdict, args.slang, args.tlang, args.outfile)
+    s = read_file(args.source)
+    t = read_file(args.target)
+    write_lett(s, t, args.slang, args.tlang, args.outfile)
