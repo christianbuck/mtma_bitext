9e43ab7ca798987b677717ec5f433d484ab7a6d9
Christian Buck <cbuck@lantis.de>
2015-05-25 20:02:43 -0400

    filter empty lines from .lett

--- baseline/lett2corpus.sh
+++ baseline/lett2corpus.sh
@@ -15,7 +15,8 @@ LOG=${LETT/lett/log}
 
 BT=/home/buck/net/build/bitextor/bin
 
-
+mv ${LETT} ${LETT}.bak
+/home/buck/net/build/DataCollection/baseline/filter_emty_text_from_lett.py < ${LETT}.bal > ${LETT}
 echo -n "LETT .. LETTR .. "
 ${BT}/bitextor-lett2lettr < ${LETT} > ${LETTR}
 echo -n "IDX .. "
@@ -30,6 +31,8 @@ echo -n "SENTS .. "
 ${BT}/bitextor-align-segments --lang1 en --lang2 fr -d ${DICT} < ${DOCS} > ${SENT} 2> ${LOG}
 echo -n "Cleaning up .. "
 rm -f ${IDX} ${LETTR} ${RIDX} ${DIST} ${DOCS} ${LOG}
+rm ${LETT}
+mv ${LETT}.bak ${LETT}
 echo "Done! "
 echo -n "EN: " 
 cut -f 3 ${SENT} | wc
